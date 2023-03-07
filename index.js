a6d5568d4ef53aee25c69d0c1439bef7718ac7c6
JR Frazier <justinfrazier@jrs-mbp-2.lan>
2023-03-07 04:39:59 -0500

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const sequelize = require("./util/database");
+const User = require("./models/user");
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // for corse
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+// test route
+app.get("/", (req, res, next) => {
+  res.send("Hello World!!!");
+});
+
+// CRUD Routes
+app.use("/users", require("./routes/users"));
+
+// error handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+// sync database
+sequelize
+  .sync()
+  .then((result) => {
+    console.log("Databese Connected");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
